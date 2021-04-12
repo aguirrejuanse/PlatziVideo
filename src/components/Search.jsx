1f4bd8831f3ae83269d721d5b9877b9df0ee3b62
aguirrejuanse <you@example.com>
2021-04-12 20:51:17 -0300

    PlatziVideo terminado <3

---
+++ src/components/Search.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../assets/styles/components/Search.scss';
+
+const MainTitleText = '¿Que quieres ver hoy?';
+const MainInputPlaceholder = 'Buscar...';
+
+const Search = () => (
+    <section className='main'>
+    <h2 className='main__title'>{MainTitleText}</h2>
+    <input
+        className='input'
+        type='text'
+        placeholder={MainInputPlaceholder}
+    />
+    </section>
+);
+
+export default Search;
