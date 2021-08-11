64586f1708ced27a992bba82ceb564e5648ef34a
aguirrejuanse <you@example.com>
2021-08-11 11:51:16 -0300

    modified strategies/basic.js, register actions and server routes

--- src/server/utils/auth/strategies/basic.js
+++ src/server/utils/auth/strategies/basic.js
@@ -2,7 +2,9 @@ const passport = require('passport');
 const { BasicStrategy } = require('passport-http');
 const boom = require('@hapi/boom');
 const axios = require('axios');
-const { config } = require('../../../config/index');
+// const { config } = require('../../../config/index');
+
+require('dotenv').config;
 
 // definimos nuestra nueva estrategia
 passport.use(
@@ -10,14 +12,14 @@ passport.use(
     try {
 
       const { data, status } = await axios({
-        url: `${config.apiUrl}/api/auth/sign-in`,
+        url: `${process.env.API_URL}/api/auth/sign-in`,
         method: "post",
         auth: {
           password,
           username: email
         },
         data: {
-          apiKeyToken: config.apiKeyToken
+          apiKeyToken: process.env.API_KEY_TOKEN
         }
       });
       
