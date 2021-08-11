2df27f257ced3fc732086b8a6bbb42123970d112
aguirrejuanse <you@example.com>
2021-08-11 10:44:03 -0300

    C:/Program Files/Git/frontend/index.js add redux-thunk

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 import App from './routes/App';
 
-const history = createBrowserHistory();
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const preloadedState = window.__PRELOADED_STATE__;
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducer, preloadedState, composeEnhancers);
+const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)));
+const history = createBrowserHistory();
 
 delete window.__PRELOADED_STATE__;
 
