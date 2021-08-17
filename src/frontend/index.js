753206671070085800702c1159039028a9ede80b
aguirrejuanse <you@example.com>
2021-08-17 19:34:20 -0300

    add utils/auth/strategies and redux thunk

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Router } from 'react-router';
+import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
 import reducer from './reducers';
 import App from './routes/App';
@@ -10,7 +11,7 @@ import App from './routes/App';
 const history = createBrowserHistory();
 const preloadedState = window.__PRELOADED_STATE__;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducer, preloadedState, composeEnhancers);
+const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)));
 
 delete window.__PRELOADED_STATE__;
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,28 +1,41 @@
 import express from 'express';
-require('dotenv').config();
 import webpack from 'webpack';
 import React from 'react';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import boom from '@hapi/boom';
+import passport from 'passport';
+import axios from 'axios';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
 import { renderRoutes } from 'react-router-config';
 import { StaticRouter } from 'react-router-dom';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../../webpack.config';
 import serverRoutes from '../frontend/routes/serverRoutes';
 import initialState from '../frontend/initialState';
 import reducer from '../frontend/reducers';
 import getManifest from './getManifest';
 import App from '../frontend/routes/App';
 
+require('dotenv').config();
+
 const { ENV, PORT } = process.env;
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Debemos hacer uso de nuestra Strategy Basic
+require('./utils/auth/strategies/basic');
+
 if (ENV === 'development') {
   console.log('Development config');
-  const webpackConfig = require('../../webpack.config');
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(webpackConfig);
   const serverConfig = { port: PORT, hot: true };
 
@@ -30,9 +43,9 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 } else {
   app.use((req, res, next) => {
-    if(!req.hashManifest) req.hashManifest = getManifest();
+    if (!req.hashManifest) req.hashManifest = getManifest();
     next();
-  })
+  });
   app.use(express.static(`${__dirname}/public`));
   app.use(helmet());
   app.use(helmet.permittedCrossDomainPolicies());
@@ -41,7 +54,7 @@ if (ENV === 'development') {
 
 const setResponse = (html, preloadedState, manifest) => {
   // const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
-  const mainStyles = manifest ? manifest['vendors.css'] : 'assets/app.css'
+  const mainStyles = manifest ? manifest['vendors.css'] : 'assets/app.css';
   const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
   const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
 
@@ -50,35 +63,93 @@ const setResponse = (html, preloadedState, manifest) => {
     <!DOCTYPE html>
       <html>
         <head>
-          <link rel="stylesheet" href="${mainStyles}" type="text/css">
+          <link rel='stylesheet' href='${mainStyles}' type='text/css'>
           <title>Platzi Video</title>
         </head>
         <body>
-          <div id="app">${html}</div>
+          <div id='app'>${html}</div>
           <script>
             window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
           </script>
-          <script src="${mainBuild}" type="text/javascript"></script>
-          <script src="${vendorBuild}" type="text/javascript"></script>
+          <script src='${mainBuild}' type='text/javascript'></script>
+          <script src='${vendorBuild}' type='text/javascript'></script>
         </body>
       </html>
   `
   );
-}
+};
 
 const renderApp = (req, res) => {
   const store = createStore(reducer, initialState);
   const preloadedState = store.getState();
   const html = renderToString(
-    <Provider store={store} >
-      <StaticRouter location={req.url} context={{}} >
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
         {renderRoutes(serverRoutes)}
       </StaticRouter>
-    </Provider>
+    </Provider>,
   );
 
   res.send(setResponse(html, preloadedState, req.hashManifest));
-}
+};
+
+app.post('/auth/sign-in', async (req, res, next) => {
+  // Obtenemos el atributo rememberMe desde el cuerpo del request
+  const { rememberMe } = req.body;
+  // console.log(req.cookies);
+  passport.authenticate('basic', (error, data) => {
+    try {
+      if (error || !data) {
+        return next(boom.unauthorized('Error la data viene vacía :('));
+      }
+
+      req.login(data, { session: false }, async (error) => {
+        if (error) {
+          return next(error);
+        }
+
+        const { token, ...user } = data;
+
+        // Si el atributo rememberMe es verdadero la expiración será en 30 dias
+        // de lo contrario la expiración será en 2 horas
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC,
+        });
+
+        // if (!config.dev) {
+        //   res.cookie('token', token, {
+        //     httpOnly: true,
+        //     secure: true,
+        //   });
+        // } else {
+        //   res.cookie('token', token);
+        // }
+
+        return res.status(200).json(user);
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
+app.post('/auth/sign-up', async (req, res, next) => {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'post',
+      data: user,
+    });
+
+    res.status(201).json({ message: 'user created' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.get('*', renderApp);
 
